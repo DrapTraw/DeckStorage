9b297c1649b42a73e5fda56acdddfa1f523645a5
DrapTraw <drapitrawpospolity@gmail.com>
2026-08-29 20:29:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -44,44 +44,7 @@ supabaseClientIndex.auth.getUser().then(function(result) {
     }
 
 });
-nav {
-    background: rgba(255, 255, 255, 0.15);
-    backdrop-filter: blur(10px);
-    padding: 1rem;
-    position: fixed;
-    width: 80%;
-    left: 50%;
-    transform: translateX(-50%);
-    top: 2rem;
-    border-radius: 30px;
-    border: 1px solid rgba(255, 255, 255, 0.5);
-    box-shadow: 0 6px 6px rgba(0, 0, 0, 0.5);
-}
 
-nav ul {
-    list-style: none;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    margin: 0;
-    padding: 0;
-}
-
-nav a {
-    color: white;
-    text-decoration: none;
-    font-size: 18px;
-    font-weight: 500;
-    padding: 10px 18px;
-    border-radius: 15px;
-    transition: all 0.3s ease;
-}
-
-nav a:hover {
-    background: rgba(255, 255, 255, 0.2);
-    transform: translateY(-3px);
-    box-shadow: 0 5px 15px rgba(255, 255, 255, 0.2);
-}
 
 supabaseClientIndex
     .from("Decki")
